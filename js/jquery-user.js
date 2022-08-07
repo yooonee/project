1e54e19b547a05c98910926790f3e183c2393123
Jiyoon Jang <wkdwldbs17327770@gmail.com>
2022-08-07 17:50:21 +0900

    first commit

---
+++ js/jquery-user.js
@@ -0,0 +1,78 @@
+    // 각각 서브메뉴가 내려오기
+// $("nav").mouseover(function() {
+//   // $(this).children(".submenu").children("ul").slideDown();
+//   // $(this).children(".submenu").children("li").slideDown();
+//   $(this).children(".submenu").stop().slideDown();
+// });
+// $("nav").mouseout(function() {
+//   // $(this).children(".submenu").children("ul").slideUp();
+//   // $(this).children(".submenu").children("li").slideDown();
+//   $(this).children(".submenu").stop().slideUp();
+// });
+
+// 서브메뉴 한번에 내려오기
+$(function () {
+    $(".nav>li").mouseover(function() {
+        // $(this).children(".submenu").children("ul").slideDown();
+        //  $(this).children(".submenu").children("li").slideDown();
+        $(".submenu").stop().slideDown();
+    });
+    $(".nav>li").mouseout(function() {
+        // $(this).children(".submenu").children("ul").slideUp();
+  // $(this).children(".submenu").children("li").slideDown();
+        $(".submenu").stop().slideUp();
+    });
+
+  
+
+
+    // fadeIn_Out
+    var x = $(".slide img");
+    var y = 3;
+    setInterval(function() {
+        y--;
+        if(y==0) {
+            x.fadeIn();
+            y = 3;
+        } else {
+            x.eq(y).fadeOut();
+        }       
+    }, 2000)
+
+
+
+const slides = document.querySelector('.slides'); //전체 슬라이드 컨테이너
+const slideImg = document.querySelectorAll('.slides li'); //모든 슬라이드들
+let currentIdx = 0; //현재 슬라이드 index
+const slideCount = slideImg.length; // 슬라이드 개수
+const prev = document.querySelector('.prev'); //이전 버튼
+const next = document.querySelector('.next'); //다음 버튼
+const slideWidth = 300; //한개의 슬라이드 넓이
+const slideMargin = 100; //슬라이드간의 margin 값
+
+//전체 슬라이드 컨테이너 넓이 설정
+slides.style.width = (slideWidth + slideMargin) * slideCount + 'px';
+
+function moveSlide(num) {
+  slides.style.left = -num * 400 + 'px';
+  currentIdx = num;
+}
+
+prev.addEventListener('click', function () {
+  /*첫 번째 슬라이드로 표시 됐을때는 
+  이전 버튼 눌러도 아무런 반응 없게 하기 위해 
+  currentIdx !==0일때만 moveSlide 함수 불러옴 */
+
+  if (currentIdx !== 0) moveSlide(currentIdx - 1);
+});
+
+next.addEventListener('click', function () {
+  /* 마지막 슬라이드로 표시 됐을때는 
+  다음 버튼 눌러도 아무런 반응 없게 하기 위해
+  currentIdx !==slideCount - 1 일때만 
+  moveSlide 함수 불러옴 */
+  if (currentIdx !== slideCount - 1) {
+    moveSlide(currentIdx + 1);
+  }
+});
+});
